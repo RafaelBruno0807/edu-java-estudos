151496604a7a29eebefc07faa40420470cd1bec5
Rafael_B <rafaelbruno210@gmail.com>
2023-10-27 15:02:19 -0400

    Loop e mudanças na estrutura

---
+++ tipos-variaveis/src/EstudoLoop.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class EstudoLoop {
+	public static void main(String[] args) {
+		String nomePrimeiro = "Rafaell";
+		String nomeSegundo = "Bruno";
+		int numPrimeiro = 11;
+		
+		System.out.println("Bom dia, vamos checar o seu nome: ");
+		
+		if (nomePrimeiro == "Rafael") {
+			do
+			{
+				System.out.println("Topper");
+			}
+			while (numPrimeiro < 10);
+		}
+		else {
+			System.out.println(nomeSegundo);
+		}
+		for (int i = 0; i <= 10; i = i + 5)
+		{
+			System.out.println(i);
+		}
+		List<String> Cars = Arrays.asList("BMW", "FORD", "AUDI");
+		
+		for(String Car : Cars) {
+			System.out.println(Car);
+		}
+}
+}
